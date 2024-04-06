65ef81454ca8216a5f6797959593ee47ffb0fe1e
Michael Loney <mloney@protonmail.com>
2024-04-06 16:26:31 -0500

    Optimize side panel resizing and prevent interference

--- content_scripts/kantbotha.js
+++ content_scripts/kantbotha.js
@@ -1,89 +1,108 @@
-/*async function checkAndRequestHostPermission(origin) {
-  const corePermissions = {
-    origins: [origin]
-  };
-  if (! await browser.permissions.contains(corePermissions)) {
-    return await browser.permissions.request(corePermissions);
-  }
-  return true;
-}
-
-checkAndRequestHostPermission('https://app.mavenlink.com/*');*/
+const verbose = false;
 
 class ResizeHelper {
-  constructor(element) {
+  constructor(element, resizeBarElement) {
     this.element = element;
-    this.animationFrameId = null;
-    this.boundHandleMouseUp = this.handleMouseUp.bind(this);
+    this.resizing = false;
     this.boundHandleMouseMove = this.handleMouseMove.bind(this);
-    this.boundDoResize = this.doResize.bind(this);
+    this.boundDoAnimationFrame = this.doAnimationFrame.bind(this);
+
+    // Use this element to block other mouse events from interfering with the resize
+    this.mouseUpBlockerElement = document.createElement('div');
+    this.mouseUpBlockerElement.style.setProperty('position', 'fixed');
+    this.mouseUpBlockerElement.style.setProperty('top', '0');
+    this.mouseUpBlockerElement.style.setProperty('left', '0');
+    this.mouseUpBlockerElement.style.setProperty('width', '100vw');
+    this.mouseUpBlockerElement.style.setProperty('height', '100vh');
+    this.mouseUpBlockerElement.style.setProperty('z-index', `9999999999999`);
+    this.mouseUpBlockerElement.style.setProperty('opacity', '0');
+    this.mouseUpBlockerElement.style.setProperty('display', 'none');
+
+    document.body.appendChild(this.mouseUpBlockerElement);
+
+    resizeBarElement.addEventListener('mousedown', this.handleMouseDown.bind(this));
+    window.addEventListener('mouseup', this.handleMouseUp.bind(this));
   }
 
   handleMouseDown(event) {
+    if (verbose) console.log('handleMouseDown');
     event.preventDefault();
     event.stopPropagation();
 
     this.clientX = event.clientX;
-    this.animationFrameId = null;
-    window.addEventListener('mouseup', this.boundHandleMouseUp);
+    this.newClientX = event.clientX;
+    this.resizing = true;
+    this.mouseUpBlockerElement.style.setProperty('display', 'block');
     window.addEventListener('mousemove', this.boundHandleMouseMove);
+    window.requestAnimationFrame(this.boundDoAnimationFrame);
   }
 
   handleMouseUp(event) {
+    if (verbose) console.log('handleMouseUp');
+    if (!this.resizing) return;
+
     event.preventDefault();
     event.stopPropagation();
 
+    this.resizing = false;
     window.removeEventListener('mousemove', this.boundHandleMouseMove);
-    window.removeEventListener('mouseup', this.boundHandleMouseUp);
+    this.clientX = null;
+    this.mouseUpBlockerElement.style.setProperty('display', 'none');
   }
 
   handleMouseMove(event) {
+    if (verbose) console.log('handleMouseMove');
     event.preventDefault();
     event.stopPropagation();
 
     this.newClientX = event.clientX;
-    if (this.animationFrameId === null) {
-      this.animationFrameId = window.requestAnimationFrame(this.boundDoResize);
-    }
   }
 
-  doResize(timestamp) {
+  doResize() {
+    if (verbose) console.log('doResize');
+    const newClientX = this.newClientX;
+    if (this.clientX === newClientX || isNaN(newClientX)) return;
     if (isNaN(this.clientX)) {
       this.clientX = this.newClientX;
       return;
     }
 
-    const distance = this.newClientX - this.clientX;
+    const distance = newClientX - this.clientX;
     let width = window.getComputedStyle(this.element).width;
     if (!width.endsWith('px')) {
       throw 'Width is not in pixels';
     }
     width = parseInt(width);
-    this.element.style.setProperty('width', `${width - distance}px`);
+    if (this.resizing)
+      this.element.style.setProperty('width', `${width - distance}px`);
 
-    this.clientX = this.newClientX;
-    this.animationFrameId = null;
+    this.clientX = newClientX;
+  }
+
+  doAnimationFrame(timestamp) {
+    if (verbose) console.log('doAnimationFrame');
+    this.doResize();
+    if (this.resizing)
+      window.requestAnimationFrame(this.boundDoAnimationFrame);
   }
 }
 
 console.log('kantbotha.js loaded');
-async function addResizeBarToSidebar() {
-  await new Promise(resolve => setTimeout(resolve, 1000));
+async function addResizeBarToSidepanel() {
   let intervalId = setInterval(() => {
-    const sidebar = document.querySelector('#side-panel');
-    if (!sidebar) return;
+    const sidepanel = document.querySelector('#side-panel');
 
-    if (sidebar.innerHTML) {
+    if (sidepanel && sidepanel.innerHTML) {
       clearInterval(intervalId);
       const resizeBar = document.createElement('div');
-      resizeBar.id = 'sidebar-resize-bar';
-      sidebar.insertBefore(resizeBar, sidebar.firstElementChild);
+      resizeBar.id = 'sidepanel-resize-bar';
+      sidepanel.insertBefore(resizeBar, sidepanel.firstElementChild);
 
-      const resizer = new ResizeHelper(sidebar);
-
-      resizeBar.addEventListener('mousedown', resizer.handleMouseDown.bind(resizer));
+      const resizer = new ResizeHelper(sidepanel, resizeBar);
+      console.log('Added resize bar');
     }
   }, 100);
 }
 
-addResizeBarToSidebar();
+addResizeBarToSidepanel();
+console.log('kantbotha.js loaded');
